d01ddc3f6ad36dd19b669f4e13c85056ce078904
Payton Turnage <paytonturnage@gmail.com>
2021-08-01 19:58:53 -0700

    no preprocess

--- preprocess.fish
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/fish
-
-set articles (git ls-files | grep -e \.md)
-for article in $articles
-  set name (echo $article | sed -e s/.md//g)
-  cat $article | podiat > $name.md.new
-  mv $name.md.new $name.md
-end
